e704acdb158e906597d400a5c2da0c73eb02692e
MelisaCan20 <127137334+MelisaCan20@users.noreply.github.com>
2023-03-06 21:57:56 +0300

    Update dataPreprocessing.py

--- dataPreprocessing.py
+++ dataPreprocessing.py
@@ -3,6 +3,15 @@ from difPy import dif
 for category in categoryNames:
     print(category)
     search = dif("Dataset/"+category,delete=True)
-
+#hata oluşturulan verilerin temizlenmesi
+import cv2
+for category in categoryNames:
+    os.chdir(category) 
+    print('=> '+category) 
+    for file in os.listdir('./'):
+        img = cv2.imread(file) 
+        if(isinstance(img, type(None))):
+            os.remove(file)
+    os.chdir('..') 
 
   
